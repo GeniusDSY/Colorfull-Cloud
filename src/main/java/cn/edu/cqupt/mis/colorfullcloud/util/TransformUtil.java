0bee0cb28fb29e05e88b6ef067f16dccf75a236b
Genius_DSY <genius_dsy@foxmail.com>
2019-08-23 16:30:42 +0800

    :art:转换工具类

---
+++ src/main/java/cn/edu/cqupt/mis/colorfullcloud/util/TransformUtil.java
@@ -0,0 +1,63 @@
+package cn.edu.cqupt.mis.colorfullcloud.util;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+/**
+ * @author :DengSiYuan
+ * @date :2019/4/21 13:40
+ * @desc :
+ */
+public class TransformUtil {
+
+    /**
+     * 一个实体类之间的转换
+     * @param src
+     * @param target
+     * @return target
+     * */
+    public static Object transformOne(Object src,Object target){
+        Method[] srcMethods = src.getClass().getMethods();
+        Method[] targetMethods = target.getClass().getMethods();
+        for (Method srcMethod : srcMethods){
+            String srcName = srcMethod.getName();
+            if(srcName.startsWith("get")){
+                try {
+                    Object result = srcMethod.invoke(src);
+                    for (Method targetMethod : targetMethods){
+                        String targetName = targetMethod.getName();
+                        if(targetName.startsWith("set") && targetName.substring(3,targetName.length())
+                                .equals((srcName.substring(3, srcName.length())))){
+                            targetMethod.invoke(target,result);
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return target;
+    }
+
+    /**
+     * 一个实体集合的转换
+     * @param src
+     * @param target
+     * @param targetClass
+     * @return list
+     * */
+    public static <S, T> List<T> transformList(List<S> src, List<T> target,
+                                              Class<?> targetClass) {
+        for (int i = 0; i < src.size(); i++) {
+            try {
+                Object object = targetClass.newInstance();
+                target.add((T) object);
+                transformOne(src.get(i), object);
+            } catch (Exception e) {
+                // 某个方法反射异常
+                continue;
+            }
+        }
+        return target;
+    }
+}
