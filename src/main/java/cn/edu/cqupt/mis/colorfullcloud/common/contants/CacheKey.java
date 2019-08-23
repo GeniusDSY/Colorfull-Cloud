9ac4bdf171ccfad6c583a7b1b90cb2bec50abdd7
Genius_DSY <genius_dsy@foxmail.com>
2019-08-23 16:41:40 +0800

    :art:缓存常量

---
+++ src/main/java/cn/edu/cqupt/mis/colorfullcloud/common/contants/CacheKey.java
@@ -0,0 +1,14 @@
+package cn.edu.cqupt.mis.colorfullcloud.common.contants;
+
+/**
+ * @author :DengSiYuan
+ * @date :2019/4/21 17:33
+ * @desc : 缓存相关的数值
+ */
+public interface CacheKey {
+
+    String USER_ID = "id";
+
+    Integer CACHE_TIME = 3600000;
+
+}
