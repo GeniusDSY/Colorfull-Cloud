d7c73db72b733cc9a3d19dd0339e0ac5fd37a255
Genius_DSY <genius_dsy@foxmail.com>
2019-08-27 16:22:25 +0800

    :fire:移除多余注解代码

--- src/main/java/cn/edu/cqupt/mis/colorfullcloud/domain/entity/UserEntity.java
+++ src/main/java/cn/edu/cqupt/mis/colorfullcloud/domain/entity/UserEntity.java
@@ -10,44 +10,44 @@ import java.sql.Date;
 /**
  * @author :DengSiYuan
  * @date :2019/5/2 13:42
- * @desc :
+ * @desc : 用户数据库实体类
  */
 @Data
 @ToString
 public class UserEntity {
 
-    @ApiModelProperty(name = "id",value = "用户ID(主键、自增）",dataType = "int")
-    private Integer id;
+    @ApiModelProperty(value = "用户ID(主键、自增）")
+    private Integer userId;
     @NotNull(message = "openid不能为空")
-    @ApiModelProperty(name = "openid",value = "用户微信openid",dataType = "string")
+    @ApiModelProperty(value = "用户微信openid")
     private String openid;
-    @ApiModelProperty(name = "nickName",value = "自定义昵称",dataType = "string")
+    @ApiModelProperty(value = "自定义昵称")
     private String nickName;
-    @ApiModelProperty(name = "avatarUrl",value = "微信头像链接")
+    @ApiModelProperty(value = "微信头像链接")
     private String avatarUrl;
-    @ApiModelProperty(name = "phone",value = "用户手机号",dataType = "string")
+    @ApiModelProperty(value = "用户手机号")
     private String phone;
-    @ApiModelProperty(name = "sparePhone",value = "备用手机号",dataType = "string")
+    @ApiModelProperty(value = "备用手机号")
     private String sparePhone;
-    @ApiModelProperty(name = "password",value = "密码（目前无用）",dataType = "string")
+    @ApiModelProperty(value = "密码（目前无用）")
     private String password;
-    @ApiModelProperty(name = "parentName",value = "家长姓名",dataType = "string")
+    @ApiModelProperty(value = "家长姓名")
     private String parentName;
-    @ApiModelProperty(name = "registerTime",value = "注册时间",dataType = "string")
+    @ApiModelProperty(value = "注册时间")
     private String registerTime;
-    @ApiModelProperty(name = "registerMac",value = "注册设备的Mac",dataType = "string")
+    @ApiModelProperty(value = "注册设备的Mac")
     private String registerMac;
-    @ApiModelProperty(name = "isDisabled",value = "是否可用(0:可用 1:不可用)",dataType = "integer")
+    @ApiModelProperty(value = "是否可用(0:可用 1:不可用)")
     private Integer isDisabled;
-    @ApiModelProperty(name = "age",value = "年龄",dataType = "integer")
+    @ApiModelProperty(value = "年龄")
     private Integer age;
-    @ApiModelProperty(name = "gender",value = "性别（1男0女）",dataType = "integer")
+    @ApiModelProperty(value = "性别（1男0女）")
     private Integer gender;
-    @ApiModelProperty(name = "homeAddress",value = "家庭住址",dataType = "string")
+    @ApiModelProperty(value = "家庭住址")
     private String homeAddress;
-    @ApiModelProperty(name = "wxNumber",value = "绑定的微信号",dataType = "string")
+    @ApiModelProperty(value = "绑定的微信号")
     private String wxNumber;
-    @ApiModelProperty(name = "receivingAddress",value = "收货地址",dataType = "string")
+    @ApiModelProperty(value = "收货地址")
     private String receivingAddress;
 
 
