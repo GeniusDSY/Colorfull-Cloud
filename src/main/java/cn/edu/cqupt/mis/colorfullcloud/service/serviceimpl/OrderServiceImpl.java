a8a618dc1eb85a61d151756aa4264da55e880560
Genius_DSY <genius_dsy@foxmail.com>
2019-08-28 00:36:18 +0800

    :sparkle:Order服务层接口实现类

---
+++ src/main/java/cn/edu/cqupt/mis/colorfullcloud/service/serviceimpl/OrderServiceImpl.java
@@ -0,0 +1,15 @@
+package cn.edu.cqupt.mis.colorfullcloud.service.serviceimpl;
+
+import cn.edu.cqupt.mis.colorfullcloud.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author :DengSiYuan
+ * @date :2019/8/27 16:50
+ * @desc :
+ */
+@Slf4j
+@Service
+public class OrderServiceImpl implements OrderService {
+}
