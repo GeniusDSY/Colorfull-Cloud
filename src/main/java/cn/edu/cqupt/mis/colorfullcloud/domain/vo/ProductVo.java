7b890b5c9ba1525b4ecbc16d3b8c557df33c1378
Genius_DSY <genius_dsy@foxmail.com>
2019-08-28 22:48:19 +0800

    :sparkles:Product产品展示实体类

---
+++ src/main/java/cn/edu/cqupt/mis/colorfullcloud/domain/vo/ProductVo.java
@@ -0,0 +1,25 @@
+package cn.edu.cqupt.mis.colorfullcloud.domain.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.List;
+
+/**
+ * @author :DengSiYuan
+ * @date :2019/8/28 18:53
+ * @desc :
+ */
+@ApiModel
+@Data
+@ToString
+public class ProductVo {
+
+    @ApiModelProperty(name = "type",value = "商品展示方式",dataType = "int")
+    private Integer type;
+    @ApiModelProperty(name = "institutionVoList",value = "机构列表",dataType = "list")
+    private List<InstitutionVo> institutionVoList;
+
+}
