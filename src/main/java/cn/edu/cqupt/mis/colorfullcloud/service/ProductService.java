e3acbda5ba766cb729607eff98abcfca84932a31
Genius_DSY <genius_dsy@foxmail.com>
2019-09-01 19:33:07 +0800

    :sparkles:增加默认与按距离返回所有产品

---
+++ src/main/java/cn/edu/cqupt/mis/colorfullcloud/service/ProductService.java
@@ -0,0 +1,33 @@
+package cn.edu.cqupt.mis.colorfullcloud.service;
+
+import cn.edu.cqupt.mis.colorfullcloud.domain.vo.InstitutionVo;
+import cn.edu.cqupt.mis.colorfullcloud.domain.vo.ProductVo;
+
+import java.util.List;
+
+/**
+ * @author :DengSiYuan
+ * @date :2019/9/1 8:50
+ * @desc : 产品服务接口类
+ */
+public interface ProductService {
+
+    /**
+     * 默认按照机构分类获取所有产品
+     * @return 产品集合
+     */
+    List<InstitutionVo> allDefaultProducts();
+
+    /**
+     * 按照距离顺序排序所有产品
+     * @return 按距离返回
+     */
+    List<InstitutionVo> allDistantProducts();
+
+    /**
+     * 分类查询所有产品
+     * @return 按类分组返回
+     */
+    List<InstitutionVo> allCategoryProducts();
+
+}
