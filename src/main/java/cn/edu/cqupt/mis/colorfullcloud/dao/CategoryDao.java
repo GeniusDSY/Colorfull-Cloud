08175ce99c984e511e91b29482655e872ccaa9d3
Genius_DSY <genius_dsy@foxmail.com>
2019-09-03 19:25:59 +0800

    :sparkles:分类数据库接口类

---
+++ src/main/java/cn/edu/cqupt/mis/colorfullcloud/dao/CategoryDao.java
@@ -0,0 +1,30 @@
+package cn.edu.cqupt.mis.colorfullcloud.dao;
+
+import cn.edu.cqupt.mis.colorfullcloud.domain.entity.CategoryEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @author :DengSiYuan
+ * @date :2019/9/2 11:23
+ * @desc :
+ */
+@Repository
+public interface CategoryDao {
+
+    /**
+     * 查询所有类别
+     * @return 返回所有类别的集合
+     */
+    List<CategoryEntity> selectAllCategories();
+
+    /**
+     * 新增类别
+     * @param type 类别名称
+     * @return true 添加成功 false 添加失败
+     */
+    Boolean insertCategory(String type);
+
+
+}
