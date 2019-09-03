b3d2f14bc9d69c387f645b83184135b1b7d6706b
Genius_DSY <genius_dsy@foxmail.com>
2019-09-03 19:27:19 +0800

    :art:改进Category类别实体类

---
+++ src/main/java/cn/edu/cqupt/mis/colorfullcloud/domain/entity/CategoryEntity.java
@@ -0,0 +1,17 @@
+package cn.edu.cqupt.mis.colorfullcloud.domain.entity;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author :DengSiYuan
+ * @date :2019/9/1 8:43
+ * @desc :
+ */
+@Data
+@ToString
+public class CategoryEntity {
+
+    private Integer categoryId;
+    private String type;
+}
