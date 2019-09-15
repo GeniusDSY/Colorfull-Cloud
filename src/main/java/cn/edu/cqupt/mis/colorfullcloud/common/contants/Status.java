f9e9d204ddfb85772704bd891041f5b25732137f
Genius_DSY <genius_dsy@foxmail.com>
2019-09-15 18:33:11 +0800

    :fire: 增加订单状态常量

--- src/main/java/cn/edu/cqupt/mis/colorfullcloud/common/contants/Status.java
+++ src/main/java/cn/edu/cqupt/mis/colorfullcloud/common/contants/Status.java
@@ -19,30 +19,9 @@ public interface Status {
     String MAP_KEY="key";
     String FROM="from";
     String TO="to";
-    /**
-     * 设置登录状态的key
-     */
-    String LOGIN_KEY = "login";
-
-    /**
-     * 登录状态为已登录的值
-     */
-    String LOGIN_ATTRIBUTE = "isLogined";
-
-    /**
-     * 登录验证码key
-     */
-    String CODE_KEY = "verifyCode";
-
-    /**
-     * session存放userId 的key
-     */
-    String USER_ID_KEY = "user";
-
-    /**
-     * 注册存放邮箱验证码的key
-     */
-    String MAIL_CODE_KEY = "mailCode";
+    Integer PAY_SUCCESS = 1;
+    Integer EVENT_OFFER = 2;
+    Integer CANCEL = 3;
 
 
 }
