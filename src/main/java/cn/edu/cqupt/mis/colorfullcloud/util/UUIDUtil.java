c526489f63098fa205079728ea534bea3de0b49d
Genius_DSY <genius_dsy@foxmail.com>
2019-09-19 21:57:06 +0800

    :fire: 更改UUID工具类

--- src/main/java/cn/edu/cqupt/mis/colorfullcloud/util/UUIDUtil.java
+++ src/main/java/cn/edu/cqupt/mis/colorfullcloud/util/UUIDUtil.java
@@ -1,5 +1,9 @@
 package cn.edu.cqupt.mis.colorfullcloud.util;
 
+import cn.edu.cqupt.mis.colorfullcloud.dao.OrderDao;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 import java.util.UUID;
 
 /**
@@ -7,10 +11,22 @@ import java.util.UUID;
  * @date :2019/9/15 15:43
  * @desc : 随机编号的生成
  */
+@Component
 public class UUIDUtil {
 
-    public static String getRandomString(){
+    @Resource
+    private OrderDao orderDao;
+
+    private static String getRandomString(){
         return String.valueOf(UUID.randomUUID());
+    }
+
+    public String getRandomOrderId(){
+        String orderId = getRandomString();
+        while (orderDao.selectOrderId(orderId) != null){
+            orderId = getRandomString();
+        }
+        return orderId;
     }
 
 }
